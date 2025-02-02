51d83991aca66c81ceb26c652c0c9831ef3864f9
John Paul Arriola <arriolajp7@gmail.com>
2025-02-02 05:05:49 -0500

    Stacknavigator corrected

--- fluint/App.js
+++ fluint/App.js
@@ -1,9 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import StackNavigator from './src/components/StackNavigator';
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <StackNavigator />
+      <StatusBar style="auto" />
       <Text>PENIS</Text>
       <StatusBar style="auto" />
     </View>
--- fluint/src/components/StackNavigator.js
+++ fluint/src/components/StackNavigator.js
@@ -1,48 +1,33 @@
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NavigationContainer } from '@react-navigation/native';
-import HomeScreen from './screens/HomeScreen';
+import HomeScreen from '../screens/HomeScreen';
 
-const Tab = createBottomTabNavigator();
+
 const Stack = createNativeStackNavigator();
 
-const BottomTabs = () => {
-    const BottomTabs = () => {
-        return (
-          <Tab.Navigator
-            screenOptions={{
-              tabBarStyle: {
-                backgroundColor: 'rgba(0,0,0,0.3)',
-                position: 'absolute',
-                bottom: 0,
-                left: 0,
-                right: 0,
-                elevation: 0,
-                // borderTopWidth: 5,
-                // borderTopColor: 'rgba(0,0,0,0.3)',
-                paddingTop: 10,
-                paddingBottom: 10,
-                height: 75,
-              },
-            }}>
-            <Tab.Screen
-              name="Home"
-              component={HomeScreen}
-              options={{
-                tabBarLabel: 'Home',
-                headerShown: false,
-                tabBarLabelStyle: { color: 'white', fontWeight: 'bold', fontSize: 12},
-                tabBarIcon: ({ focused }) =>
-                  focused ? (
-                    <Entypo name="home" size={30} color="white" />
-                  ) : (
-                    <AntDesign name="home" size={30} color="white" />
-                  ),
-              }}
-            />
-            
-          </Tab.Navigator>
-        );
-      };
+const StackNavigator = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Home" // Set initial screen
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: '#f4511e',
+          },
+          headerTintColor: '#fff',
+          headerTitleStyle: {
+            fontWeight: 'bold',
+          },
+        }}>
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ title: 'Welcome to Home' }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
 
-    };
+export default StackNavigator;
